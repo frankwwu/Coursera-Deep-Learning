41eedcff323d6540318f7c2102c02272d087b8d3
frankwwu <frankwwu@hotmail.com>
2022-11-19 13:56:56 -0500

    keras_preprocessing

--- 5. Sequence Models/Week 1. Recurrent Neural Networks/shakespeare_utils.py
+++ 5. Sequence Models/Week 1. Recurrent Neural Networks/shakespeare_utils.py
@@ -5,7 +5,7 @@ from keras.models import Model, load_model, Sequential
 from keras.layers import Dense, Activation, Dropout, Input, Masking
 from keras.layers import LSTM
 from keras.utils.data_utils import get_file
-from keras.preprocessing.sequence import pad_sequences
+from keras_preprocessing.sequence import pad_sequences
 import numpy as np
 import random
 import sys
